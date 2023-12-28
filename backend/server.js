f04b256f3963ac3c338333b038e93c52e7727db0
Safiyah <safiyahmasud@gmail.com>
2023-12-28 21:02:00 +0100

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT
+
+// Connect to database
+connectDB()
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.send('Hey')
+	res.status(200).send({ message: 'Welcome To The Support Desk API' })
 })
 
 // Routes
